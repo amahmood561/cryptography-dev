1da47edac208cff1eec834922e24d19124e72267
as ma <amahmood561@gmail.com>
2018-01-28 12:02:44 -0500

    commit of initial test scripts

---
+++ cryptographytest2.py
@@ -0,0 +1,10 @@
+from cryptography.fernet import Fernet # Put this somewhere safe!
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+token
+print token
+'...'
+f.decrypt(token)
+print f.decrypt(token)
+'A really secret message. Not for prying eyes.'
